6c78d8e09248af178370ab826fa9d9cca68ba8e4
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2025-08-04 03:18:18 +0100

    feat: paddle moving

--- main.lua
+++ main.lua
@@ -6,21 +6,61 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
+
+	smallFont = love.graphics.newFont("font.ttf", 8)
+	scoreFont = love.graphics.newFont("font.ttf", 32)
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
 		vsync = true,
 	})
+
+	p1Score = 0
+	p2Score = 0
+
+	p1Y = 30
+	p2Y = VIRTUAL_HEIGHT - 50
 end
 
-function love.update(dt) end
+function love.update(dt)
+	if love.keyboard.isDown("w") then
+		p1Y = p1Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown("s") then
+		p1Y = p1Y + PADDLE_SPEED * dt
+	end
+
+	if love.keyboard.isDown("up") then
+		p2Y = p2Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown("down") then
+		p2Y = p2Y + PADDLE_SPEED * dt
+	end
+end
 
 function love.draw()
 	push:apply("start")
-	love.graphics.printf("Hello World!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, "center")
+	love.graphics.setColor(40, 45, 52, 255)
+
+	love.graphics.setFont(smallFont)
+	love.graphics.printf("Hello World!", 0, 20, VIRTUAL_WIDTH, "center")
+
+	love.graphics.setFont(scoreFont)
+	love.graphics.print(tostring(p1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+	love.graphics.print(tostring(p2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+	-- left paddle
+	love.graphics.rectangle("fill", 10, p1Y, 5, 20)
+
+	-- right paddle
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 10, p2Y, 5, 20)
+
+	-- ball
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
 	push:apply("end")
 end
 
